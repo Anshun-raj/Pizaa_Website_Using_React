8017269159e2669be75def44b87cf9a66dc382ea
Anshu Raj <60781096+Anshun-raj@users.noreply.github.com>
2023-10-02 10:08:47 +0530

    Update Contact.js

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,133 +1,77 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Contact.css";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-function Contact() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [age, setAge] = useState("");
-  const [address, setAddress] = useState("");
-  const [message, setMessage] = useState("");
-  const [confirm, setConfirm] = useState("");
-  const [nameErr, setNameErr] = useState(false);
-  const [emailErr, setEmailErr] = useState(false);
-  const [ageErr, setAgeErr] = useState(false);
-  const [addressErr, setAddressErr] = useState(false);
-  const [messageErr, setMessageErr] = useState(false);
+export const Form = () => {
+  const schema = yup.object().shape({
+    name: yup.string().required("please enter valid data"),
+    email: yup.string().email().required("please enter valid data"),
+    age: yup
+      .number()
+      .positive()
+      .integer()
+      .min(18)
+      .required("please enter valid data"),
+    address: yup.string().min(6).max(30).required(""),
+    message: yup.string().min(4).max(50).required(""),
+  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
-  function handleForm(e) {
-    if (
-      name.length < 4 ||
-      age.length < 2 ||
-      message.length < 4 ||
-      email.length < 6 ||
-      address.length < 5
-    ) {
-      alert("please enter valid data");
-    } else {
-      alert("submitted");
-      setConfirm("Data has been submitted!");
-    }
-    e.preventDefault();
-  }
-
-  function handleName(e) {
-    const item = e.target.value;
-    if (item.length < 4) {
-      setNameErr(true);
-    } else {
-      setNameErr(false);
-    }
-    setName(item);
-  }
-
-  function handleEmail(e) {
-    const item = e.target.value;
-    if (item.length < 6) {
-      setEmailErr(true);
-    } else {
-      setEmailErr(false);
-    }
-    setEmail(item);
-  }
-  function handleAge(e) {
-    const item = e.target.value;
-    if (item.length < 2) {
-      setAgeErr(true);
-    } else {
-      setAgeErr(false);
-    }
-    setAge(item);
-  }
-  function handleAddress(e) {
-    const item = e.target.value;
-    if (item.length < 5) {
-      setAddressErr(true);
-    } else {
-      setAddressErr(false);
-    }
-    setAddress(item);
-  }
-  function handleMessage(e) {
-    const item = e.target.value;
-    if (item.length < 4) {
-      setMessageErr(true);
-    } else {
-      setMessageErr(false);
-    }
-    setMessage(item);
+  const onClick=(data)=>{
+    console.log(data)
   }
 
   return (
     <div className="contact">
       <h2>Contact Us</h2>
-      <form className="form" onSubmit={handleForm}>
+      <form onSubmit={handleSubmit(onClick)}>
         <input
           type="text"
-          placeholder="enter your name"
-          onChange={handleName}
+          name="name"
+          placeholder="name.."
+          {...register("name")}
         />
-        {nameErr ? <small style={{ color: "red" }}>name not valid</small> : ""}
+        <p style={{ color: "red" }}>{errors.name?.message}</p>
         <input
           type="text"
-          placeholder="enter your email"
-          onChange={handleEmail}
+          name="email"
+          placeholder="email.."
+          {...register("email")}
         />
-        {emailErr ? (
-          <small style={{ color: "red" }}>email not valid</small>
-        ) : (
-          ""
-        )}
+        <p style={{ color: "red" }}>{errors.email?.message}</p>
         <input
-          type="number"
-          placeholder="enter your age"
-          min="21"
-          max="40"
-          onChange={handleAge}
+          type="text"
+          name="age"
+          placeholder="age.."
+          {...register("age")}
         />
-        {ageErr ? <small style={{ color: "red" }}>age not valid</small> : ""}
+        <p style={{ color: "red" }}>{errors.age?.message}</p>
         <input
-          type="address"
-          placeholder="enter your address"
-          onChange={handleAddress}
+          type="text"
+          name="address"
+          placeholder="address.."
+          {...register("address")}
         />
-        {addressErr ? (
-          <small style={{ color: "red" }}>address not valid</small>
-        ) : (
-          ""
-        )}
-        <input type="text" placeholder="message..." onChange={handleMessage} />
-        {messageErr ? (
-          <small style={{ color: "red" }}>message not valid</small>
-        ) : (
-          ""
-        )}
-        <button type="submit" className="btn1">
-          Submit Data
-        </button>
-        <h5 style={{ color: "red" }}>{confirm}</h5>
+        <p style={{ color: "red" }}>{errors.address?.message}</p>
+        <input
+          type="text"
+          name="message"
+          placeholder="message.."
+          {...register("message")}
+        />
+        <p style={{ color: "red" }}>{errors.message?.message}</p>
+        <button type="submit" className="btn1">Submit Data</button>
       </form>
     </div>
   );
-}
+};
 
-export default Contact;
+export default Form;
